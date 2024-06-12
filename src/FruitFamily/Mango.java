98ea6a4fb0579f306a2bba6f568d5ddec642bde2
Kushal Kharel <kushalkharel@kushals-mbp-2.mynetworksettings.com>
2024-06-12 10:01:09 -0400

    sum function addes

--- src/FruitFamily/Mango.java
+++ src/FruitFamily/Mango.java
@@ -8,9 +8,13 @@ public class Mango {
         // jsp <-> serlvet <->server <-> DB
         
         //this is my first class
-        
-      
+  sum(1,2);      
+      System.out.println( sum(1,2));
 
     }
 
+	public static int sum(int a, int b) {
+		return a+b;
+	}
+	
 }
