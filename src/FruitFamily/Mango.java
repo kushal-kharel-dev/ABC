e43684ecfaa4a3fae37d0e326cfc82114b04558b
Kushal Kharel <kushalkharel@kushals-mbp-2.mynetworksettings.com>
2024-06-12 09:28:23 -0400

    first commit

---
+++ src/FruitFamily/Mango.java
@@ -0,0 +1,14 @@
+package FruitFamily;
+
+public class Mango {
+	
+	public static void main(String args[])
+    {
+        System.out.println("Hello, World");
+        // jsp <-> serlvet <->server <-> DB
+        
+      
+
+    }
+
+}
